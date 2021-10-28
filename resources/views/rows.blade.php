63742eed9c1a0aca55c3ed80b002ff75dd98a674
Bienvenu Murenzi <bmurenzi25@gmail.com>
2021-10-28 13:50:35 +0200

    Blade Template Test

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -21,13 +21,27 @@
                         <tbody>
                             @foreach ($users as $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
-                                    <td>{{ $user->name }}</td>
-                                    {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
-                                    <td>{{ $user->created_at }}</td>
-                                </tr>
+                                @if ($loop->even)
+                                    <tr class="bg-red-100">
+                                        <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                        <td>{{ $user->name }}</td>
+                                        @if ($loop->first)
+                                            <td class="font-bold">{{ $user->email }}</td>
+                                        @else
+                                            <td>{{ $user->email }}</td>
+                                        @endif)
+                                        <td class="font-bold">{{ $user->email }}</td>
+                                        <td>{{ $user->created_at }}</td>
+                                    </tr>
+                                @else
+                                    <tr>
+                                        <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                        <td>{{ $user->name }}</td>
+                                        {{-- Task: only the FIRST row should have email with "font-bold" --}}
+                                        <td class="font-bold">{{ $user->email }}</td>
+                                        <td>{{ $user->created_at }}</td>
+                                    </tr>
+                                @endif
                             @endforeach
                         </tbody>
                     </table>

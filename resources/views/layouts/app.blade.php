3213d60932844987f4ff241bcd846edce721fa28
Guillaume Leon <guillaume.leon2000@gmail.com>
2021-10-28 21:37:33 +0200

    solver the 8 challenges

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+			@include('layouts.navigation')
 
             <!-- Page Heading -->
             <header class="bg-white shadow">

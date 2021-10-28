b1b02dd83b2c65d20bd6c90f6116a782844977dd
Paulos Tadesse <paulosta952@gmail.com>
2021-10-28 14:32:24 +0300

    all Tests passed !

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -19,14 +19,14 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($users as $user)
+                            @foreach ($users as $key=>$user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                <tr class="@if($key % 2 != 0) bg-red-100 @endif">
+                                    <td>{{ ++$key }}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
-                                    <td>{{ $user->created_at }}</td>
+                                    <td class="@if($key == 1) font-bold @endif">{{ $user->email }}</td>
+                                        <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach
                         </tbody>

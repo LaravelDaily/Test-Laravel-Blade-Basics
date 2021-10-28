eb0e7a98bfd05e0cac64bedc6c11a7f4d2c1640c
Farhan <raditzfarhan@gmail.com>
2021-10-28 13:56:27 +0800

    Missed out content section

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
 
+@section('content')
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -11,3 +12,4 @@
         </div>
     </div>
 </div>
+@endsection

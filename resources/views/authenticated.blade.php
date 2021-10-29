78fa0211499857415b7487ea5d6591d3db703869
ayoubmehd <safi-_-gio-raja@live.fr>
2021-10-29 18:31:52 +0100

    ayoubmehd challenge solved

--- resources/views/authenticated.blade.php
+++ resources/views/authenticated.blade.php
@@ -11,8 +11,12 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     {{-- Task: add a condition to show correct text --}}
                     {{-- If user is logged in, show their email --}}
-                    Yes, I am logged in as [insert_user_email_here].
+                    @auth
+                    Yes, I am logged in as {{ auth()->user()->email }}.
+                    @endauth
+                    @guest
                     No, I am not logged in.
+                    @endguest
                 </div>
             </div>
         </div>

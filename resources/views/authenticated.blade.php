ba4e76f0ea943a6491d91e94190001a4f3abfd73
Lee Weiquan <weiquan@togalimited.com>
2021-10-29 12:51:20 +0800

    View challenge

--- resources/views/authenticated.blade.php
+++ resources/views/authenticated.blade.php
@@ -11,8 +11,30 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     {{-- Task: add a condition to show correct text --}}
                     {{-- If user is logged in, show their email --}}
-                    Yes, I am logged in as [insert_user_email_here].
-                    No, I am not logged in.
+                    @auth
+                        <div class="flex items-center justify-between">
+                            <div class="flex-1">
+                                <h3 class="text-lg leading-6 font-medium text-gray-900">
+                                    Yes, I am logged in as {{ auth()->user()->email }}
+                                </h3>
+                                <p class="mt-1 text-sm leading-5 text-gray-500">
+                                    {{ __('You are logged in!') }}
+                                </p>
+                            </div>
+                        </div>
+
+                    @endauth
+
+                    @guest
+                        <div class="flex items-center justify-between">
+                            <div class="flex-1">
+                                <h3 class="text-lg leading-6 font-medium text-gray-900">
+                                    No, I am not logged in.
+                                </h3>
+                                <p class="mt-1 text-sm leading-5 text-gray-500">
+                                    {{ __('You are not logged in!') }}
+                                </p>
+                    @endguest
                 </div>
             </div>
         </div>

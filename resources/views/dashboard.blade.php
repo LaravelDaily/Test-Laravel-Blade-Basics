0bd5f000adb8d58e0f24e737fd7e40a04b141ed4
kirillsamuilov <3025889+kirillsamuilov@users.noreply.github.com>
2021-10-29 13:37:05 +0200

    Challenge completed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+
+    <x-slot name="metaTitle">
+        Blade Test
+    </x-slot>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}

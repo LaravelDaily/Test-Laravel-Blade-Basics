a00fce47f0fffee4e274ac521a717e8f104c405c
m-rizwan-ashlar <m-rizwan@ashlarglobal.com>
2021-11-02 12:32:56 +0500

    blade tasks

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    {{ $metaTitle = 'Blade Test' }}
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}

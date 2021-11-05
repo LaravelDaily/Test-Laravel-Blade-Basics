aad37b644a70bea8501480e19fc6273e30037521
SusmoySenGupta <susmoy.cse@gmail.com>
2021-11-05 19:39:22 +0600

    completed tasks

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     {{-- Task: change the layout from layouts/app.blade.php --}}
                     {{-- to layouts/main.blade.php --}}
+                    Please change layout.
                 </div>
             </div>
         </div>

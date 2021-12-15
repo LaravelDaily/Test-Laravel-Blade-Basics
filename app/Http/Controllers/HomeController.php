2782c14ce305d79b370dbca0d7a0cbf7424dcd46
Hina <92708031+Hina326@users.noreply.github.com>
2021-12-15 17:56:53 +0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,24 +1,46 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Http\Requests;
 
 use App\Models\User;
 
 class HomeController extends Controller
 {
+		public function store(Request $request)
+    {
+       
+        $this->validate($request, [
+        'name' => 'required',
+        'email' => 'required',
+        'password' => 'required',
+        
+        ]);
+
+        $user = new User;
+        $user ->name=$name->name;
+        $user ->email=$email->email;
+        $user ->password=$password->password;
+        $user  ->save();
+
+        return redirect('users');
+    }
+
     // Task 1. Change the Controller code to pass the variable to the View
-    public function users()
+    public function users(request $request)
     {
         $usersCount = User::count();
 
-        return view('users');
+        return view('users',['usersCount'=>$usersCount]);
+	
     }
 
     // Task 2. Change the View code so alert would not show on the screen
     public function alert()
     {
         $text = '<script>alert("I am a security alert, your task is to remove me.");</script>';
-
+		
         return view('alert', compact('text'));
     }
 
@@ -26,6 +48,7 @@ class HomeController extends Controller
     public function table()
     {
         $users = User::all();
+		
 
         return view('table', compact('users'));
     }

324d8daf9ad5685853f5edc76dcaba7d8e1c9df0
Marius <marius.austr@gmail.com>
2021-12-27 23:53:58 +0000

    complete quiz

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-app-layout :metaTitle="Blad Test">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}

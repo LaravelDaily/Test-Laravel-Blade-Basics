1a3c019dea852646072a8f21d4a633d193a7488e
zyrusinso <52569449+zyrusinso@users.noreply.github.com>
2022-01-02 02:17:06 +0800

    Done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
+                    Blade Test
                 </div>
             </div>
         </div>

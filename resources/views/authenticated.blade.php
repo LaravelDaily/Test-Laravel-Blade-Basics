4aebfc956bf63f5b35ef077bbb9a132bbaf7fef0
Vikram singh <44375710+vickyjodha@users.noreply.github.com>
2022-02-01 20:17:35 +0530

    Update authenticated.blade.php

--- resources/views/authenticated.blade.php
+++ resources/views/authenticated.blade.php
@@ -14,7 +14,7 @@
                     @guest
                     No, I am not logged in.
                     @elseguest
-                     Yes, I am logged in as {{auth()->user()->email}}.
+                     Yes, I am logged in as {{Auth::user()->email}}.
                     @endguest
                     
                 </div>

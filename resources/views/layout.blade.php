9bf2b79b0e4ef216fef74ae554e1380e70ab231f
edoorinaldoo@gmail.com <edoorinaldoo@gmail.com>
2022-03-02 01:05:59 +0700

    done

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,21 +1,14 @@
 @extends('layouts.main')
-
 @section('content')
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Layout') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-
-                </div>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
+                {{-- Task: change the layout from layouts/app.blade.php --}}
+                {{-- to layouts/main.blade.php --}}
+                Please change layout.
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
 @endsection

390aeba586a06c017cc35c32a921954db0e15c9f
Hossein <hb.hossein10@gmail.com>
2022-03-05 21:09:16 +0330

    complete blade

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,4 +1,6 @@
-<x-app-layout>
+@extends('layouts.main')
+
+@section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -10,4 +12,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection

31cc95a3e578b646c53ccc6fdad5b31beeed3cbc
Admin <aziz89131983189@gmail.com>
2022-04-10 18:55:24 +0700

    fixed app-layout components

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -13,6 +13,6 @@ class AppLayout extends Component
      */
     public function render()
     {
-        return view('layouts.app',['metaTitle' => 'Blade Template']);
+        return view('layouts.app',['metaTitle' => 'Blade Test']);
     }
 }

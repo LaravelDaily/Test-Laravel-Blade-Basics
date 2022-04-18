8fc9aea926cb019dd2d604457a415e385be4630f
Nelutato <purepeper@gmail.com>
2022-04-18 19:10:06 +0200

    view's with passed test's

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -32,13 +32,9 @@
                                     <td>{{ $user->name }}</td>
 
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td 
-                                        @if($loop->first) 
-                                            class="font-bold"
-                                        @endif> 
-                                    {{ $user->email }}</td>
+                                    <td @if($loop->first) class="font-bold" @endif> {{ $user->email }}</td>
                                     {{-- !!!! Test failed here when i added spaces here :) !!!!!!!--}}
-
+                                    
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach

2abd5ff9b3635e576b49ffa2ad7e44c8934251f2
Prashant Malla <prashant.malla977@gmail.com>
2022-05-08 12:06:53 +0545

    test completed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->share('metaTitle', 'Meta Title');
+        view()->share('metaTitle', 'Blade Test');
     }
 }
--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -21,23 +21,14 @@
                         <tbody>
                             @foreach ($users as $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                @if ($loop->even)
-                                    <tr class="bg-red-100">
-                                @else
-                                    <tr>
-                                @endif
+                                    <tr @if($loop->even) class="bg-red-100" @endif>
                                     <td>
                                         {{-- Task: add row number here: 1, 2, etc. --}}
                                         {{ $loop->iteration }}
                                     </td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    @if ($loop->first)
-                                    <td class="font-bold">
-                                    @else
-                                    <td>
-                                    @endif
-                                    {{ $user->email }}</td>
+                                    <td @if($loop->first) class="font-bold" @endif>{{ $user->email }}</td>
                                     
                                     <td>{{ $user->created_at }}</td>
                                 </tr>

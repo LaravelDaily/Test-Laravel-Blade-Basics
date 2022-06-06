de6eeb5dd79d7c27c783fdeca9179c9e3d93c8fa
Khanh Chung <95897626+craftzmask@users.noreply.github.com>
2022-06-06 15:03:07 -0700

    Update authenticated.blade.php

--- resources/views/authenticated.blade.php
+++ resources/views/authenticated.blade.php
@@ -13,7 +13,7 @@
                     {{-- If user is logged in, show their email --}}
                     @auth
                         Yes, I am logged in as {{ auth()->user()->email }}.
-                    @else
+                    @elseauth
                         No, I am not logged in.
                     @endauth
                 </div>

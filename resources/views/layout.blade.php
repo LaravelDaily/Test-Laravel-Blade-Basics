7105e2b4d5712ac160bef3e9ee1cb790e3b78d8d
edoorinaldoo@gmail.com <edoorinaldoo@gmail.com>
2022-06-30 23:56:52 +0700

    Task Completed

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
             <div class="p-6 bg-white border-b border-gray-200">
                 {{-- Task: change the layout from layouts/app.blade.php --}}
                 {{-- to layouts/main.blade.php --}}
-                {{-- Please change layout. --}}
+                Please change layout.
             </div>
         </div>
     </div>

5097d4883ba9872aa190b7537f4fae4b55c94cc2
Priceless-P <prisca.priceless11@gmail.com>
2022-09-26 22:50:10 +0100

    Correction

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -1,43 +1,39 @@
 <x-app-layout>
-<x-slot name="header">
-<h2 class="font-semibold text-xl text-gray-800 leading-tight">
-{{ __('Users') }}
-</h2>
-</x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
 
-<div class="py-12">
-<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-<div class="p-6 bg-white border-b border-gray-200">
-    <table>
-        <thead>
-            <tr>
-                <th>Row Number</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Registered at</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $user)
-                {{-- Task: only every second row should have "bg-red-100" --}}
-                <tr class="@if($loop->even) bg-red-100 @endif">
-
-                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $user->name }}</td>
-                    {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                        {{-- This is the first iteration --}}
-                    <td class=" @if ($loop->first)font-bold @else">{{ $user->email }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->created_at }}</td>
-                </tr>
-
-            @endforeach
-        </tbody>
-    </table>
-</div>
-</div>
-</div>
-</div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Row Number</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Registered at</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                {{-- Task: only every second row should have "bg-red-100" --}}
+                                <tr class="@if ($loop->even) bg-red-100 @endif">
+                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    {{-- Task: only the FIRST row should have email with "font-bold" --}}
+                                    <td class="@if ($loop->first) font-bold @endif">{{ $user->email }}</td>
+                                    <td>{{ $user->created_at }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>

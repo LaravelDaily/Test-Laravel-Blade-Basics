20406a0545b3d7a0fefa93795b73a5b743ee6fa5
Timur <timurcool650@gmail.com>
2022-10-22 22:54:27 +0600

    ssdfsd

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -20,18 +20,17 @@
                         {{-- Task: add the loop here to show users, or the row "No content" --}}
                         <tbody>
                         @foreach($users as $user)
-                            @if(isset($user))
                                 <tr>
                                     <td>{{ $user->name }}</td>
                                     <td>{{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
-                            @else
-                                <tr>
-                                    <td colspan="3">No content.</td>
-                                </tr>
-                            @endif
                         @endforeach
+                        @if(!isset($users))
+                            <tr>
+                                <td colspan="3">No content.</td>
+                            </tr>
+                        @endif
                         </tbody>
                     </table>
                 </div>

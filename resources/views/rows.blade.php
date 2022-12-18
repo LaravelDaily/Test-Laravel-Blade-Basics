69c7d09664f18ed23b4b5951c79791626350d4af
erikbocil <erikluis3006@gmail.com>
2022-12-18 21:41:08 +0700

    Exercise

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -21,7 +21,7 @@
                         <tbody>
                             @foreach ($users as $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr @class(['bg-red-100' => $loop->even])>
+                                <tr @classs(['bg-red-100'=> $loop->even])>
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}

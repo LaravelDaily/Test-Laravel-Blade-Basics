4bba65a9d6153dbec380f6be9d62750fc140812e
GlaDdos <kamil.kawa@gmail.com>
2023-02-02 22:44:56 +0100

    test blade basics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
+            @include('layouts.navigation', ['metaTitle' => $metaTitle])
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">

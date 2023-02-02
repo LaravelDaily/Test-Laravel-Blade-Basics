cc5d071f01f6bd1db40dd2edd2fccf3f2eb1da4b
GlaDdos <kamil.kawa@gmail.com>
2023-02-02 23:02:04 +0100

    Corrected errors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         {{-- Task: edit one file to pass $metaTitle as "Blade Test" to all views --}}
-        <title>{{ $metaTitle ?? 'Laravel' }}</title>
+        <title>{{ $metaTitle ?? 'Blade Test' }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -19,7 +19,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation', ['metaTitle' => $metaTitle])
+            @include('layouts.navigation')
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">

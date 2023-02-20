60c2d99e9791cf09976f4abc0282f286a8669d2a
tomasspisak <tomasspisak@gmail.com>
2023-02-20 16:13:14 +0100

    Blade basics completed

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -21,11 +21,11 @@
                         <tbody>
                             @foreach ($users as $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                <tr class="@if ($loop->even) bg-red-100 @endif">
+                                    <td>{{ $loop->iteration }}{{-- Task: add row number here: 1, 2, etc. --}}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
+                                    <td class="@if ($loop->first) font-bold @endif">{{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach

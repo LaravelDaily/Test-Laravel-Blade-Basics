ac095ef045d0503293626c42fe47057ce8e3fd44
Eburu Dearie <eburujacintha@gmail.com>
2023-03-19 23:48:27 +0200

    Updated the test

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -6,7 +6,7 @@ use Illuminate\View\Component;
 
 class AppLayout extends Component
 {
-    /**
+    /** 
      * Get the view / contents that represents the component.
      *
      * @return \Illuminate\View\View

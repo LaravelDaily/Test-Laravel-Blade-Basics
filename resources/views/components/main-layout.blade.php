80412d22e1f99f5bae7b58c5c3622742f7a45ce5
Hussayn <hussaynabdsamad07@gmail.com>
2023-03-21 08:23:59 +0100

    Initial commit

---
+++ resources/views/components/main-layout.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+</div>
--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -19,16 +19,19 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($users as $user)
-                                {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
-                                    <td>{{ $user->name }}</td>
-                                    {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
-                                    <td>{{ $user->created_at }}</td>
-                                </tr>
-                            @endforeach
+                            @foreach ($users as $i => $user)
+                            {{-- Task: only every second row should have "bg-red-100" --}}
+                            <tr class="{{ $i % 2 == 1 ? 'bg-red-100' : '' }}">
+                                <td>{{ $i + 1 }}</td>
+                                <td>{{ $user->name }}</td>
+                                {{-- Task: only the FIRST row should have email with "font-bold" --}}
+                                <td class="{{ $i % 2 == 0 ? 'font-bold' : '' }}">{{ $user->email }}</td>
+                                <td>{{ $user->created_at }}</td>
+                            </tr>
+                        @endforeach
+                            {{-- 'text-gray-500' => ! $isActive, --}}
+
+
                         </tbody>
                     </table>
                 </div>

c1af8d2a22f436274f5d3a266ed6a690c7268c39
si-eng <brownkida79@gmail.com>
2023-03-23 15:36:56 +0000

    Changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,5 @@
-<x-app-layout>
-    @extends('layouts.main')
+@extends('layouts.main')
+@section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -11,4 +11,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection

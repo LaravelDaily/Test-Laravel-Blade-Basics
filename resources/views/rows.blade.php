f689346ddbe8bf8a9c2478b47a064c561ce16614
Ben Cullen <benjaminjcullen1@gmail.com>
2023-04-05 13:38:20 +0100

    first crack at all tasks

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -19,16 +19,16 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($users as $user)
-                                {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
-                                    <td>{{ $user->name }}</td>
-                                    {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
-                                    <td>{{ $user->created_at }}</td>
-                                </tr>
-                            @endforeach
+                        @foreach ($users as $index => $user)
+                            {{-- Task: only every second row should have "bg-red-100" --}}
+                            <tr @if($index % 2 == 1) class="bg-red-100" @endif>
+                                <td>{{ $index + 1 }}</td> {{-- Task: add row number here: 1, 2, etc. --}}
+                                <td>{{ $user->name }}</td>
+                                {{-- Task: only the FIRST row should have email with "font-bold" --}}
+                                <td @if($index == 0) class="font-bold" @endif>{{ $user->email }}</td>
+                                <td>{{ $user->created_at }}</td>
+                            </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>

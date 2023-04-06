b5bd2301ad1685ca45bdbee763811922abb22ca4
hagerlabib <88290234+hagerlabib@users.noreply.github.com>
2023-04-06 04:24:06 +0200

    blade test

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -18,14 +18,17 @@
                             </tr>
                         </thead>
                         {{-- Task: add the loop here to show users, or the row "No content" --}}
-                        <tbody>
+                       <tbody>
+                            @forelse ($users as $user)
                             <tr>
                                 <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
                                 <td>{{ $user->created_at }}</td>
                             </tr>
                             <tr>
+                                @empty
                                 <td colspan="3">No content.</td>
+                            @endforelse                              
                             </tr>
                         </tbody>
                     </table>

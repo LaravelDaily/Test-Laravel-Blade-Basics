61ab489685e6c9c59e0d956794561b8c184d4643
kareem-hub <kareemehab968@gmail.com>
2023-04-09 16:55:50 +0200

    app service provider fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider

dbf3510243cf4fdf28cfb39e4820e4060da2c87c
Ivan Golubev <golubevivan0500@gmail.com>
2023-04-09 13:56:20 +0300

    Task 7 complete

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        View::share('metaTitle', 'Blade Test');
     }
 }

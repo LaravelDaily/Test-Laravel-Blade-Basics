f274e2f32695f06b383e5fc8f6e9868baa05138a
kareem-hub <kareemehab968@gmail.com>
2023-04-09 16:52:46 +0200

    sloved tasks

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::share('metaTitle', 'Blade Test');
     }
 }

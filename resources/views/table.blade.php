62791706f1ea90726dc5630efd4db477b8690397
inam925 <97148236+inam925@users.noreply.github.com>
2023-04-13 08:35:43 +0500

    Update table.blade.php

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -18,16 +18,23 @@
                             </tr>
                         </thead>
                         {{-- Task: add the loop here to show users, or the row "No content" --}}
-                        <tbody>
-                            <tr>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>{{ $user->created_at }}</td>
-                            </tr>
-                            <tr>
-                                <td colspan="3">No content.</td>
-                            </tr>
-                        </tbody>
+                        @foreach ($users as $user)
+                            <tbody>
+                                @isset($user)
+                                    <tr>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->created_at }}</td>
+                                    </tr>
+                                @endisset
+
+                                @empty($user)
+                                    <tr>
+                                        <td colspan="3">No content.</td>
+                                    </tr>
+                                @endempty
+                            </tbody>
+                        @endforeach
                     </table>
                 </div>
             </div>

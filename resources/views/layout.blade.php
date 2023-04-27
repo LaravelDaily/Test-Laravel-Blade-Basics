54c401e2bb7d9dae9de2217c4380aa312ef237be
ariyantobahar <33273280+ariyantobahar@users.noreply.github.com>
2023-04-27 16:26:34 +0700

    darto

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,4 +1,4 @@
-<x-main-layout>
+@extends('layouts.main')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -10,4 +10,3 @@
             </div>
         </div>
     </div>
-</x-main-layout>

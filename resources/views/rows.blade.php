5664d93a41d2f7feec96bf9c6607d69a2a9f663d
lucidlynxx <dzakysyahrizal04@gmail.com>
2023-05-11 20:18:35 +0700

    third test

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -29,7 +29,7 @@
                                 <td>{{ $loop->iteration }}{{-- Task: add row number here: 1, 2, etc. --}}</td>
                                 <td>{{ $user->name }}</td>
                                 {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                @if ($user[0]->id)
+                                @if ($user[0])
                                 <td class="font-bold">{{ $user->email }}</td>
                                 @else
                                 <td>{{ $user->email }}</td>

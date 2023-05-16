bdeb23e648aa6550f02b797832e56aa8738b7658
Anton <anton.c@globedreamers.com>
2023-05-16 18:20:34 +0200

    first 3 tasks

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -8,10 +8,12 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @if($text =='other alert')
                 <div class="p-6 bg-white border-b border-gray-200">
                     {!! $text !!}
                     Your task is to change the code of alert.blade.php, to avoid that JavaScript alert.
                 </div>
+                @endif
             </div>
         </div>
     </div>

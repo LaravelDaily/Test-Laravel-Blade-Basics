9701ac4cf0ab270785d5114137d13bfb12484a2b
wailwinaung-dev <129765158+wailwinaung-dev@users.noreply.github.com>
2023-05-25 13:43:32 +0630

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     
 @endsection
-{{-- <x-app-layout> --}}
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -15,5 +14,4 @@
             </div>
         </div>
     </div>
-{{-- </x-app-layout> --}}
 @endsection

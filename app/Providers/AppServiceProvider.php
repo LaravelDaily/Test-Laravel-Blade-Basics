b8d3de418ddfd38370978f9baa2acb53a497b45d
Wai Lwin Aung <you@example.com>
2023-05-25 19:40:50 +0630

    fixed metaTitle 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->share('metaTitle', 'MyWebsite');
+        view()->share('metaTitle', 'Blade Test');
     }
 
 }

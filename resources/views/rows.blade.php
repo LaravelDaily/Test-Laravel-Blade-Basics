2c9a277bddde10c20b2462eb548f0e355ccf3d0d
sothylor <panha.saloeun@gmail.com>
2023-05-27 19:05:41 +0700

    Update no content in rows

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    @if ($users->count())
                     <table>
                         <thead>
                             <tr>
@@ -32,6 +33,11 @@
                             @endforeach
                         </tbody>
                     </table>
+                    @else
+                        <tr>
+                            <td colspan="3">No content.</td>
+                        </tr>
+                    @endif
                 </div>
             </div>
         </div>

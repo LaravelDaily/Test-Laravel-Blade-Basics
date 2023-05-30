2f343d0a13e0cb6c783bf7a6b69b8f54df97f5e4
Doniyor Raximov <31414369+Don734@users.noreply.github.com>
2023-05-30 17:48:51 +0500

    Update-1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,13 +11,13 @@ class HomeController extends Controller
     {
         $usersCount = User::count();
 
-        return view('users');
+        return view('users', compact('usersCount'));
     }
 
     // Task 2. Change the View code so alert would not show on the screen
     public function alert()
     {
-        $text = '<script>alert("I am a security alert, your task is to remove me.");</script>';
+        $text = '<noscript>alert("I am a security alert, your task is to remove me.");</noscript>';
 
         return view('alert', compact('text'));
     }
@@ -26,7 +26,7 @@ class HomeController extends Controller
     public function table()
     {
         $users = User::all();
-
+        
         return view('table', compact('users'));
     }
 

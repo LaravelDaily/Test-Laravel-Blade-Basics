805a70d5980fffe33539fbcbc95e79f3b88aee12
vqminh <vuquocminh99@gmail.com>
2023-05-31 23:09:03 +0700

    submit blade test

--- resources/views/authenticated.blade.php
+++ resources/views/authenticated.blade.php
@@ -9,10 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    {{-- Task: add a condition to show correct text --}}
-                    {{-- If user is logged in, show their email --}}
-                    Yes, I am logged in as [insert_user_email_here].
-                    No, I am not logged in.
+                    @auth
+                        Yes, I am logged in as {{ auth()->user()->email }}.
+                    @endauth
+
+                    @guest
+                        No, I am not logged in.
+                    @endguest
                 </div>
             </div>
         </div>

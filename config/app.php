785f8432d60ad19d1bfff7e4c87dc0b0e9da9f21
mike <mghossain.42@gmail.com>
2023-05-31 13:32:27 +0300

    blades Test

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'metaTitle' => env('META_TITLE'),
+    'metaTitle' => env('META_TITLE', 'Blade Test'),
 
     /*
     |--------------------------------------------------------------------------

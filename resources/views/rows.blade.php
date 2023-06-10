74add5dcd1a67a8848c9165d249135b069aec5a4
payerrony <payerrony@gmail.com>
2023-06-10 23:42:04 +0600

    2 task done

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -21,10 +21,15 @@
                         <tbody>
                             @foreach ($users as $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
+                                @if($loop->even)
                                 <tr class="bg-red-100">
+                                @else
+                                <tr>
+                                @endif
                                     <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
+                                
                                     <td class="font-bold">{{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
                                 </tr>

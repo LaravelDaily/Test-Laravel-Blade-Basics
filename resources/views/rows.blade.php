9818729cba5442c2f7df31520e5c495ddd980b16
olashojaa <olashojaa>
2023-06-20 15:54:05 +0300

    task done

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -21,11 +21,11 @@
                         <tbody>
                             @foreach ($users as $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                <tr class="<?php if($loop->iteration%2==0) echo 'bg-red-100';?>">
+                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}{{$loop->iteration}}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
+                                    <td class="<?php if($loop->first) echo 'font-bold';?>">{{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach

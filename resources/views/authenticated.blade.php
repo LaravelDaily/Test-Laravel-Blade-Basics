ce98e3d42da521b67ce0195c880deb3cdcd02a5b
datasandwich <jarekettl98@gmail.com>
2023-07-12 00:01:02 +0100

    six

--- resources/views/authenticated.blade.php
+++ resources/views/authenticated.blade.php
@@ -11,8 +11,11 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     {{-- Task: add a condition to show correct text --}}
                     {{-- If user is logged in, show their email --}}
-                    Yes, I am logged in as [insert_user_email_here].
+                    @if(auth()->check())
+                    Yes, I am logged in as {{ auth()->user()->name }}.
+                    @else
                     No, I am not logged in.
+                    @endif
                 </div>
             </div>
         </div>

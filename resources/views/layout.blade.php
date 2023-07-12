5eb3b71b56270e51eadd02606d59bcc5d837db0e
datasandwich <jarekettl98@gmail.com>
2023-07-12 08:21:46 +0100

    fourteen

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,4 @@
 @extends('main')
-@section('content')
-<x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -12,6 +10,5 @@
             </div>
         </div>
     </div>
-</x-app-layout>
-@endsection
+
 

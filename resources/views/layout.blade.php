872e12d53123d4731a4fadcb383bb8189483471b
datasandwich <jarekettl98@gmail.com>
2023-07-12 08:19:35 +0100

    thirteen

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,16 +1,17 @@
+@extends('main')
+@section('content')
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     {{-- Task: change the layout from layouts/app.blade.php --}}
-                    @extends('main')
                     {{-- to layouts/main.blade.php --}}
-                    @section('content')
                     Please change layout.
-                    @endsection
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+@endsection
+

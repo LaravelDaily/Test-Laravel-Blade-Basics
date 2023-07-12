4369a9ec53efc7050555c75bb40704ae3fdeec99
datasandwich <jarekettl98@gmail.com>
2023-07-12 08:06:02 +0100

    eleven

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     {{-- Task: change the layout from layouts/app.blade.php --}}
+                    @extends('main')
                     {{-- to layouts/main.blade.php --}}
                     Please change layout.
                 </div>

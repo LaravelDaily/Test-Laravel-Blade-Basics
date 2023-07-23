fc111014893170423543a8429acda4b5ea1b87d9
ahmed-alfy <alalfyay@gmail.com>
2023-07-23 16:53:48 +0300

    edit the layout.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
-<html lang="<?php str_replace('_', '-', app()->getLocale()) ?>">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="<?php csrf_token() ?>">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         {{-- Task: edit one file to pass $metaTitle as "Blade Test" to all views --}}
-        <title><?= $metaTitle ?? 'Laravel' ?></title>
+        <title>{{ $metaTitle ?? 'Laravel' }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="<?php asset('css/app.css') ?>">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Scripts -->
-        <script src="<?php asset('js/app.js') ?>" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -24,13 +24,13 @@
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <?= $header ?>
+                    {{ $header }}
                 </div>
             </header>
 
             <!-- Page Content -->
             <main>
-                <?= $slot ?>
+                {{ $slot }}
             </main>
         </div>
     </body>

d70513b9c5fb5876f186fb55d6753b0685a634e8
ahmed-alfy <alalfyay@gmail.com>
2023-07-23 16:24:51 +0300

    Blade test laravel daily

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+
                     <table>
                         <thead>
                             <tr>
@@ -19,16 +20,27 @@
                         </thead>
                         {{-- Task: add the loop here to show users, or the row "No content" --}}
                         <tbody>
+                            @if(count($users)>0)
+                            @foreach ($users as $user )
+
                             <tr>
                                 <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
                                 <td>{{ $user->created_at }}</td>
                             </tr>
+                            @endforeach
+                            @else
+
                             <tr>
                                 <td colspan="3">No content.</td>
                             </tr>
+                            @endif
+
+
+
                         </tbody>
                     </table>
+
                 </div>
             </div>
         </div>

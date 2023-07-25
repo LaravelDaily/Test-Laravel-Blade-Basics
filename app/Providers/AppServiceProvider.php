630326a741c8dbd084e59581db388380b90a27b3
Goudmane Oualid <walidlioon.09@gmail.com>
2023-07-25 17:45:45 +0100

    OG

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $metaTitle = "Blade Test";
+        View::share('metaTitle', $metaTitle);
     }
 }

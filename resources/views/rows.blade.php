0764d0a591e6a7f22de6132a382edcaec7cfb7e8
Odame <kwesi.danquah@daaseng.com>
2023-07-28 11:25:51 +0100

    laravel blade basics

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -19,16 +19,22 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($users as $user)
-                                {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
-                                    <td>{{ $user->name }}</td>
-                                    {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
-                                    <td>{{ $user->created_at }}</td>
+                            @if (count($users) > 0)
+                                @foreach ($users as $key => $user)
+                                    {{-- Task: only every second row should have "bg-red-100" --}}
+                                    <tr class="{{ $key % 2 == 0 ? '' : 'bg-red-100' }}">
+                                        <td>{{-- Task: add row number here: 1, 2, etc. --}}{{ $key + 1 }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        {{-- Task: only the FIRST row should have email with "font-bold" --}}
+                                        <td class="{{ $key === 0 ? 'font-bold' : '' }}">{{ $user->email }}</td>
+                                        <td>{{ $user->created_at }}</td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <tr>
+                                    <td colspan="3">No content.</td>
                                 </tr>
-                            @endforeach
+                            @endif
                         </tbody>
                     </table>
                 </div>

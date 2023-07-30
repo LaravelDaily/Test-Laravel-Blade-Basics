175ee2d8f91d52891553490eced91d0a9d1f8f1f
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-07-30 22:13:07 +0330

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.main')
 
-@section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -12,4 +11,4 @@
             </div>
         </div>
     </div>
-@endsection
+

557e1f27bbb44264a1eb22a3486fa52d62c85265
Michał <40917477+miccom1977@users.noreply.github.com>
2023-09-03 21:51:19 +0200

    Update rows.blade.php

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -25,7 +25,7 @@
                                     <td>{{-- Task: add row number here: 1, 2, etc. --}}{{ $loop->iteration }}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td @ if ($loop->first) class="font-bold" @endif>{{ $user->email }}</td>
+                                    <td @if ($loop->first) class="font-bold" @endif>{{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach

55446013e2fd374af373aacd9e16fb8801239d98
Samsun Maarif <samsunmaarif62@gmail.com>
2023-09-12 10:30:21 +0700

    update

--- resources/views/authenticated.blade.php
+++ resources/views/authenticated.blade.php
@@ -11,11 +11,11 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     {{-- Task: add a condition to show correct text --}}
                     {{-- If user is logged in, show their email --}}
-                    @if(!$user->isEmpty())
-                        Yes, I am logged in as {{ $user->email }}.
+                    @auth
+                        <p>Yes, I am logged in as {{ auth()->user()->email }}.</p>
                     @else
-                        No, I am not logged in.
-                    @endif
+                        <p>No, I am not logged in.</p>
+                    @endauth
                 </div>
             </div>
         </div>

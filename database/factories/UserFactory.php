97b31c99ac8a6d597c999131fb4cf717a1f9707a
dhanar98 <dhanasekarravi98@gmail.com>
2023-09-27 16:49:06 +0530

    Test Cases Verified - All Passed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$Gv4gtdkZRKVcMPAHX4urg.r6TE7Qd1iGX497otP0VDUR1j0CNnBz2', // password
             'remember_token' => Str::random(10),
         ];
     }

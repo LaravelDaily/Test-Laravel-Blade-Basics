cc83d847f316c0783c2d4df89f44bc33fd7868e4
Ikram Khizer <ikramkhizer@gmail.com>
2023-10-11 15:57:58 +0500

    Solution by Ikram Khizer

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <table>
+                    <table class="table-fixed">
                         <thead>
                             <tr>
                                 <th>Name</th>
@@ -19,14 +19,17 @@
                         </thead>
                         {{-- Task: add the loop here to show users, or the row "No content" --}}
                         <tbody>
+                            @forelse($users as $user)
                             <tr>
                                 <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
                                 <td>{{ $user->created_at }}</td>
                             </tr>
+                            @empty
                             <tr>
                                 <td colspan="3">No content.</td>
                             </tr>
+                            @endforelse
                         </tbody>
                     </table>
                 </div>

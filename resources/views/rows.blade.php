154a94c5c77400737c77feaeefd1cd643d4bf2ab
Abdullah Wasif <aef1803@gmail.com>
2023-10-20 02:52:07 +0600

    6 task done, 2 is incomplete

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -19,13 +19,13 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($users as $user)
+                            @foreach ($users as $key => $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                <tr @if ( ($key + 1) % 2 == 0) class="bg-red-100" @endif>
+                                    <td>{{ $key + 1 }}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
+                                    <td @if ($key == 0) class="font-bold" @endif>{{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach

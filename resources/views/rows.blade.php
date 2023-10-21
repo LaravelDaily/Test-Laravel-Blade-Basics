b2e8f5ca92c719c71c7ccfddf122624f0ca8f04e
Courtney Joy <148651418+courtneyjoyj@users.noreply.github.com>
2023-10-21 15:33:14 -0500

    testing laravel blade basics

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Users') }}
+{{--            {{ __('Users') }}--}}
+            {{ $metaTitle ?? 'Laravel' }}
         </h2>
     </x-slot>
 
@@ -21,11 +22,19 @@
                         <tbody>
                             @foreach ($users as $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                @if ($loop->even)
+                                    <tr class="bg-red-100">
+                                @else
+                                    <tr>
+                                @endif
+                                    <td>{{ $loop->iteration }}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
+                                    @if ($loop->first)
+                                        <td class="font-bold">{{ $user->email }}</td>
+                                    @else
+                                        <td>{{ $user->email }}</td>
+                                    @endif
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach

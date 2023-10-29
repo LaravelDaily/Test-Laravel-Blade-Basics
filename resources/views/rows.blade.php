5cff783f9617120e5a3714e7306f5eaecfbf8a06
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-10-29 15:58:36 +0800

    Update rows.blade.php

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -19,13 +19,19 @@
                             </tr>
                         </thead>
                         <tbody>
+                            @php 
+                                $isActive=true;
+                            @endphp
                             @foreach ($users as $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                {{$isActive=!$isActive}}
+                                <tr @class(['bg-red-100'=>$isActive])>
+                                    <td>{{$loop->iteration}}{{-- Task: add row number here: 1, 2, etc. --}}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
+                                    @if($loop->first)
+                                        <td class="font-bold">{{ $user->email }}</td>
+                                    @endif
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach

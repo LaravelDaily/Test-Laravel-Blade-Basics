9ecef27807c35166089bfdeb4efc01f20106894d
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-11-03 12:38:47 +0800

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,7 +15,7 @@
 @endsection
 
 
-{{--
+/*
 <x-app-layout>
 <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -29,5 +29,5 @@
         </div>
     </div>
 </x-app-layout>
---}}
+*/
 

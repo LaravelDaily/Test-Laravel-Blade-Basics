3162b8030c269733049be8b476e3a407adecef89
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-11-19 16:29:13 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        View::share($metaTitle,'Blade Test');
+        View::share('metaTitle','Blade Test');
     }
 }

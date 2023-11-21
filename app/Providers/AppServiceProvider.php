9c649744014a00ac5248dc12cd95cd31df641b6b
alaeddineth <theljanialaeddine@gmail.com>
2023-11-21 21:25:29 +0100

    "blade_test_tasks"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->share('metaTitle', 'Blade Test');
+
     }
 }
--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -19,13 +19,13 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($users as $user)
+                            @foreach ($users as $key => $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                <tr class="{{ $key % 2 == 1 ? 'bg-red-100' : '' }}">
+                                    <td>{{ $key + 1 }}</td>{{-- Task: add row number here: 1, 2, etc. --}}
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
+                                    <td class="{{ $key == 0 ? 'font-bold' : '' }}">{{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach

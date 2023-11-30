9830a33820139bd531d9abc4206e534d179d72ec
geldibayev <geldibayevbega@gmail.com>
2023-11-30 20:16:21 +0500

    all tests have passed

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -21,11 +21,11 @@
                         <tbody>
                             @foreach ($users as $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                <tr class="{{ $loop->index % 2 == 1 ? 'bg-red-100' : '' }}">
+                                    <td>{{ $loop->index + 1 }}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
+                                    <td class="{{ $loop->first ? 'font-bold' : '' }}">{{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach

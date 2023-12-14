6ac88c29f5e0e92c05c605b79704335a43da0db8
John Loyd Lao <67764717+johnloydlao@users.noreply.github.com>
2023-12-14 17:38:09 +0800

    Update authenticated.blade.php

--- resources/views/authenticated.blade.php
+++ resources/views/authenticated.blade.php
@@ -10,9 +10,13 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     {{-- Task: add a condition to show correct text --}}
+                   @auth
+                    Yes, I am logged in as {{ $user->email }}.
+                    @endauth
                     {{-- If user is logged in, show their email --}}
-                    Yes, I am logged in as [insert_user_email_here].
+                    @guest
                     No, I am not logged in.
+                    @endguest
                 </div>
             </div>
         </div>

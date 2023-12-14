1dc44236d55bf7c061fdd6da8717cf9a09a58fee
John Loyd Lao <67764717+johnloydlao@users.noreply.github.com>
2023-12-14 19:14:59 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,12 +7,14 @@ use App\Models\User;
 class HomeController extends Controller
 {
     // Task 1. Change the Controller code to pass the variable to the View
-    public function users()
+     public function users()
     {
         $usersCount = User::count();
+        $meta_title = 'Blade Test';
 
-         return view('users', ['usersCount' => 'usersCount']);
+        return view('users', ['usersCount' => 'usersCount', 'meta_title']);
 
+        
     }
 
     // Task 2. Change the View code so alert would not show on the screen

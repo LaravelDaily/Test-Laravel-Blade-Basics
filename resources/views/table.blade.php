41a1fa462daaf94ec8c26be240266e363b778d5d
aexionn <corpsesbag@gmail.com>
2023-12-24 22:42:39 +0800

    test

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -18,16 +18,20 @@
                             </tr>
                         </thead>
                         {{-- Task: add the loop here to show users, or the row "No content" --}}
+                        @foreach ($users as $user)
                         <tbody>
                             <tr>
                                 <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
                                 <td>{{ $user->created_at }}</td>
                             </tr>
+                            @empty($user)
                             <tr>
                                 <td colspan="3">No content.</td>
                             </tr>
+                            @endempty
                         </tbody>
+                        @endforeach
                     </table>
                 </div>
             </div>

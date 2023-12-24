8771735f7f1c58c3891671a1399fae0c465bc0f9
aexionn <corpsesbag@gmail.com>
2023-12-24 23:08:01 +0800

    test four

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,7 @@
-<x-app-layout>
-    @extends('layouts.main')
+@extends('layouts.main')
 
-    @section('content')
+@section('content')
+<x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">

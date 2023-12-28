e5e1d94290db1950f33f9a199bf386d57b090761
aexionn <corpsesbag@gmail.com>
2023-12-28 20:22:57 +0800

    test two

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts/main')
 
-@section('name')
-    
+@section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">

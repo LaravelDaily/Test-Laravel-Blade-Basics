f19273c9b091f2249d352801fed84363b4342d5d
Karim Ashraf <60367614+Karim129@users.noreply.github.com>
2024-01-05 04:55:59 +0200

    #

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,7 +36,6 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->configureRateLimiting();
-
         $this->routes(function () {
             Route::prefix('api')
                 ->middleware('api')

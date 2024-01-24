8bf5249c7b1d8ed053d5ffa687e2b625be553f40
Ahmad Essam <72272968+ahmadessam5@users.noreply.github.com>
2024-01-24 16:09:12 +0200

    8th task

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.main')
-<x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -11,4 +10,3 @@
             </div>
         </div>
     </div>
-</x-app-layout>

a92eaa7cf22fe2e385ee557ab846927a6c70a6bf
Ahmad Essam <72272968+ahmadessam5@users.noreply.github.com>
2024-01-24 14:55:27 +0200

    task 2 solved

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,9 +17,9 @@ class HomeController extends Controller
     // Task 2. Change the View code so alert would not show on the screen
     public function alert()
     {
-        $text = '<script>alert("I am a security alert, your task is to remove me.");</script>';
+       // $text = '<script>alert("I am a security alert, your task is to remove me.");</script>';
 
-        return view('alert', compact('text'));
+        return view('alert');
     }
 
     // Task 3. Change the View code to show users, or row "No content" if 0 users

e9ce0a2724d788da4e3d15410a1e765a62bf6dcd
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2024-01-27 19:06:18 +0800

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,17 +1,3 @@
-// <x-app-layout>
-//     <div class="py-12">
-//         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-//             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-//                 <div class="p-6 bg-white border-b border-gray-200">
-//                     {{-- Task: change the layout from layouts/app.blade.php --}}
-//                     {{-- to layouts/main.blade.php --}}
-//                     Please change layout.
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-// </x-app-layout>
-
 @extend('layouts.main')
 
 @section('content')
@@ -26,3 +12,6 @@
         </div>
     </div>
 @endsection
+
+
+

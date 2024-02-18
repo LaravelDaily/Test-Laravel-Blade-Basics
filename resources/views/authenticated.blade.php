4dc576530aef092e8145937db132c0dc7944f81a
Xuan <108394451+xuanxuan27@users.noreply.github.com>
2024-02-18 17:59:06 +0800

    Update authenticated.blade.php

--- resources/views/authenticated.blade.php
+++ resources/views/authenticated.blade.php
@@ -12,7 +12,7 @@
                     {{-- Task: add a condition to show correct text --}}
                     {{-- If user is logged in, show their email --}}
                     @if (auth()->check())
-                        Yes, I am logged in as {{auth()->user->email}}.
+                        Yes, I am logged in as {{auth()->user()->email}}.
                     @else
                         No, I am not logged in.
                     @endif

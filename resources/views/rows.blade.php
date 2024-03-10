746a8b162e8975c821cd1fd1356374e67de972df
LizaTiukh <lizatiuh2014@gmail.com>
2024-03-10 12:20:02 +0200

    rows styling applied

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -19,15 +19,16 @@
                             </tr>
                         </thead>
                         <tbody>
+                            @php $count = 1; @endphp
                             @foreach ($users as $user)
-                                {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
-                                    <td>{{ $user->name }}</td>
-                                    {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
-                                    <td>{{ $user->created_at }}</td>
-                                </tr>
+                            <tr @if ($count % 2==0) class="bg-red-100" @endif>
+                                <td>{{ $count}}</td>
+                                <td>{{ $user->name }}</td>
+                                {{-- Task: only the FIRST row should have email with "font-bold" --}}
+                                <td @if ($count==1) class="font-bold" @endif>{{ $user->email }}</td>
+                                <td>{{ $user->created_at }}</td>
+                            </tr>
+                            @php $count++; @endphp
                             @endforeach
                         </tbody>
                     </table>
